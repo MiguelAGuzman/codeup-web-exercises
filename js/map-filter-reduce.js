a825e1864c8158f0a583e2dddd8d92e5604549d1
mike <miguel.a.guzman0009@gmail.com>
2022-06-24 16:48:27 -0500

    mfr ex

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -35,3 +35,40 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+let users3l = users.filter(function(user) {
+    return user.languages.length >= 3;
+});
+console.log(users3l);
+
+let userEmail = users.map(function (e){
+    return e.email + 1;
+})
+console.log(userEmail);
+
+// Use .reduce to get the total years of experience from the list of users.
+//     Once you get the total of years you can use the result to calculate the
+// average.
+
+const avgYears = users.reduce((sum,years) => {
+    return sum + years.yearsOfExperience / users.length
+}, 0);
+console.log(avgYears);
+
+// Use .reduce to get the longest email from the list of users.
+
+// const findLongestEmail = ((str) => {
+    // let strSplit = str.split(' ');
+
+//     const longestEmail = users.reduce((longest, currentEmail) => {
+//          return longest.length > currentEmail.length ? longest:currentEmail
+//     }, "");
+//     // return longestEmail;
+// }
+// console.log(longestEmail);
+
+const longestEmail = users.reduce(
+    (longest, currentEmail) =>
+        longest.length > currentEmail.length ? longest : currentEmail.length,
+    0);
+console.log(longestEmail);
