89363b08def4a364c778109981b0fa7aeff1a02c
mike <miguel.a.guzman0009@gmail.com>
2022-06-28 09:11:37 -0500

    practice

---
+++ js/Practice.js
@@ -0,0 +1,46 @@
+// let hello = "hello world";
+// let myName = "bob";
+// let myLastName = "smith";
+//
+// if(myName === "bob" || myLastName === "smith") {
+//     console.log("I know you");
+// } else {
+//     console.log("I don't know you")
+// }
+//
+//
+//
+// for (let i =0; i < myLastName.length; i++) {
+//     console.log(myLastName.charAt(i));
+// }
+//
+//
+// for (let i = myLastName.length - 1; i >= 0; i--) {
+//     console.log(myLastName.charAt(i));
+// }
+
+// let done = false;
+// //start command loop
+// while (!done) {
+//     //show a menu of options
+//     console.log("Hi there");
+//     console.log("enter 'quit to exit");
+//     console.log("enter '1' to print myValue");
+//     console.log("enter '2' to print myValue");
+//     //wait for user to pick
+//     //get input from user
+//     const userChoice = prompt("Enter your choice");
+//
+//     //process the userChoice
+//     if(userChoice === "quit" || userChoice === "stop") {
+//         done = true;
+//     } else if (userChoice === "1") {
+//         console.log(`myValue = ${myValue}`);
+//     } else if ()
+//
+    //start loop over if we didn't exit
+// }
+
+//after quitting, keep going from here
+// console.log("program is over");
+
