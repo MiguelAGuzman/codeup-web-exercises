fc24ad76007d7dffbc54c550109193b290ce6bbe
mike <miguel.a.guzman0009@gmail.com>
2022-07-08 15:34:25 -0500

    started dom traverse ex

---
+++ js/parks.js
@@ -0,0 +1,13 @@
+// const arches = document.getElementsByClassName('arches-btn');
+//
+// arches.addEventListener('')
+
+// let arches = document.querySelector("#arches-btn");
+// arches.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     arches.lastElementChild.style.backgroundColor = "yellow";
+// })
+
+// arches-btn.addEventListener("click", function() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+// });
